cd68e461fe51f9fdf2ce520555eede8d052a8c9e
joze-net <50345325+joze-net@users.noreply.github.com>
2019-10-25 22:55:47 -0500

    se creo enlace a estilos css

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -4,6 +4,7 @@ class Login extends CI_Controller{
 	public function index(){
 		$data = array('titulo' => 'Inicio' , 'mensaje' => 'Hola mundo desde un array');
 		$this->load->view('Ingreso.html',$data);
+		
 	}
 }
 
