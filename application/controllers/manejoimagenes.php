7c60561b61d810c8652bb283001eacf203e3cf7c
joze-net <50345325+joze-net@users.noreply.github.com>
2019-11-21 19:36:13 -0500

    no podia hacer push

---
+++ application/controllers/manejoimagenes.php
@@ -0,0 +1,37 @@
+<?php 
+
+/**
+ * 
+ */
+class Manejoimagenes extends CI_Controller
+{
+	
+	function cargaImagen(){
+		$img=$this->input->post('imagen');
+
+		 $imagen='upload';
+		 $config['upload_path']="upload/";//carpeta donde se gurdara las imgenes
+		 $config['file_name']="nombre_archivo";//esto es opcional
+		 $config['allowed_types']="jpg|png";//los datos permitidos
+		 $config['maxsize']='5000';//kb
+		 $config['max_width']='2000';
+		 $config['max_heigth']='2000';
+
+		 $this->load->library('upload',$config);//cargamos la libreria donde esta la carpeta y pasamos la configuracion
+
+		 if(!$this->upload->do_upload($imagen)){//esto es para subir el archivo devuelve true o false
+
+
+		 	$data['uploadError']=$this->upload->display_errors();
+		 	echo $this->upload->display_errors();
+		 	echo "string";
+		 	return;
+		 }
+
+		 $data['uploadSuccess']=$this->upload->data();
+		 var_dump($this->upload->data());//si es exotoso
+		
+	}
+}
+
+ ?>
